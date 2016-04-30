9dc1844ed411160f6a1ab68c7c03b36293e6afa5
ZiqiXiong <zx012013@mymail.pomona.edu>
2016-04-30 12:18:39 -0700

    updated

--- datawrangling.R
+++ datawrangling.R
@@ -8,13 +8,8 @@ user_keywords = purchase %>% filter(!grepl('Parking|parking',primary_act_name))%
   group_by(purch_party_lkup_id,primary_act_name) %>%
   dplyr::summarise(freq=sum(tickets_purchased_qty))
 
-names(user_keywords) = c('id','keywords','num_events')
+names(user_keywords) = c('user_id','events','tickets')
 user_keywords$id = as.factor(user_keywords$id)
 old_levels = levels(user_keywords$id)
 user_keywords$id=mapvalues(user_keywords$id, from = old_levels, to = seq(1,length(old_levels)))
 
-set.seed(1)
-rmatrix  <-  matrix(sample(0:100, 16), ncol=4)
-
-dimnames(rmatrix) <- list(rownames(rmatrix, do.NULL = FALSE, prefix = "row"),
-                          colnames(rmatrix, do.NULL = FALSE, prefix = "col"))
