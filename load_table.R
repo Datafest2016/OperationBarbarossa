b48143c9212c55d8e5d7decb7245e08cbeb76a78
ZiqiXiong <zx012013@mymail.pomona.edu>
2016-04-30 12:13:22 -0700

    new user_keywords

--- load_table.R
+++ load_table.R
@@ -1,4 +1,4 @@
 install.packages('data.table')
-purchase <- data.table::fread('approved_data_purchase-v5.csv')
-behavior <- data.table::fread('approved_ga_data_v2.csv')
-ad <- data.table::fread('approved_adwords_v3.csv')
+purchase <- data.table::fread('data/approved_data_purchase-v5.csv')
+behavior <- data.table::fread('data/approved_ga_data_v2.csv')
+ad <- data.table::fread('data/approved_adwords_v3.csv')
